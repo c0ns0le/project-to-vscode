f8745938baa2d56d6ea0c0c1265f5a528b25493e
Rafael Borring <rafaelnborring@gmail.com>
2018-07-17 11:28:07 -0300

    Adicionado pylint-django e removido flake8

--- core/run.py
+++ core/run.py
@@ -7,28 +7,24 @@ from git import Repo
 
 
 class ExtendedEnvBuilder(EnvBuilder):
-
     def __init__(self, *args, **kwargs):
-
         super().__init__(*args, **kwargs)
 
     def post_setup(self, context):
-
         self.activate = '{}/bin/activate'.format(context.env_dir)
-        self.packages = ['pylint', 'pep8', 'flake8', 'autopep8']
-
+        self.packages = ['autopep8', 'pep8', 'pylint', 'pylint-django']
         system('source {} && pip install -U pip'.format(self.activate))
-        system('source {} && pip install {}'.format(self.activate, ' '.join(
-            x for x in self.packages)))
-
+        system('source {} && pip install {}'.format(
+            self.activate, ' '.join(x for x in self.packages)
+        ))
         if path.isdir('{}/../.git'.format(context.env_dir)) and system(
-                'git show-branch develop'):
+                'git show-branch develop'
+        ):
             system('cd {}/../ && git checkout develop'.format(context.env_dir))
-
         if path.isfile('{}/../requirements.txt'.format(context.env_dir)):
             system('source {} && pip install -r {}/../requirements.txt'.format(
-                self.activate, context.env_dir))
-
+                self.activate, context.env_dir
+            ))
         with open('{}/bin/activate'.format(context.env_dir), 'a') as file_act:
             file_act.write('\n')
             file_act.write('HISTFILE="$VIRTUAL_ENV/../.bash_history"\n')
@@ -37,62 +33,51 @@ class ExtendedEnvBuilder(EnvBuilder):
 
 
 class Create(object):
-
     def __init__(self, arg):
-
         self.path_workspace = '{}/{}'.format(getenv('HOME'), 'Projects')
         self.arg = arg
-
         if match('^https://github.com/', self.arg):
             self.git = True
             self.name = self.arg.split('/')[-1][:-4]
         else:
             self.git = False
             self.name = self.arg
-
         self.path_project = '{}/{}'.format(self.path_workspace, self.name)
 
     def folder(self):
-
         makedirs('{}'.format(self.path_project), exist_ok=True)
 
     def git_clone(self):
-
         if self.git:
             Repo.clone_from(self.arg, '{}'.format(self.path_project))
 
     def venv(self):
-
         ExtendedEnvBuilder(with_pip=True).create('{}/.venv'.format(
-            self.path_project))
+            self.path_project
+        ))
 
     def shortcut(self):
-
         with open('{}/code.desktop'.format(self.path_project), 'w') as vscode:
             vscode.write('[Desktop Entry]\n')
             vscode.write('Type = Application\n')
             vscode.write('Name = Visual Studio Code\n')
             vscode.write('Exec = bash -c ')
             vscode.write('"source {}/.venv/bin/activate && code {}"\n'.format(
-                self.path_project, self.path_project))
+                self.path_project, self.path_project
+            ))
             vscode.write('Icon = code\n')
-
         chmod('{}/code.desktop'.format(self.path_project), 0o775)
 
 
 if __name__ == '__main__':
-
     start = Create(argv[1])
     start.folder()
-
     try:
         start.git_clone()
     except:
         pass
-
     try:
         start.venv()
     except:
         pass
-
     start.shortcut()
